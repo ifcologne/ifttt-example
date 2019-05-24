6b08a2093865b9928db595156df963b48d9d79aa
Ingo Friepoertner <ifcologne@gmail.com>
2019-05-24 11:53:25 +0200

    Update setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,3 +1,7 @@
 "use strict";
 const { db } = require("@arangodb");
 const { context } = require("@arangodb/locals");
+
+if (!db._collection(context.configuration.collectionname) && !context.configuration.collectionname === "") {
+  db._createDocumentCollection(context.configuration.collectionname);
+}
