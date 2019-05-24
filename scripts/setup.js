0ff64a210170918533a852844b1ff73c7eb3959e
Ingo Friepoertner <ifcologne@gmail.com>
2019-05-24 10:52:12 +0200

    drop dependency

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,3 @@
 "use strict";
 const { db } = require("@arangodb");
 const { context } = require("@arangodb/locals");
-
-if (!context.collection("tweets")) {
-  db._createDocumentCollection(context.collectionName("tweets"));
-}
